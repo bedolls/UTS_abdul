c39f1ee6fc9bf2e068f7f619271d598934a583d5
A <147518138+bedolls@users.noreply.github.com>
2024-11-23 10:30:24 +0700

    menambahkan dan mengubah di form.php

--- modul_event/form.php
+++ modul_event/form.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Data Jurusan</title>
+    <title>Data Event</title>
     <link rel="stylesheet" href="../css/bootstrap.css">
     <link rel="stylesheet" href="../css/all.css">
 </head>
@@ -17,18 +17,26 @@
         <div class="col-8 m-auto">
             <div class="card">
             <div class="card-header">
-                <h3 class="float-start">Form Data Jurusan</h3>
+                <h3 class="float-start">Form Data Event</h3>
                 
             </div>
             <div class="card-body">
             <form action="proses.php" method="post">
                 <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Kode Jurusan</label>
-                    <input type="text" name="kode" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    <label for="exampleInputEmail1" class="form-label">Nama Event</label>
+                    <input type="text" name="nama_event" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
                 </div>
                 <div class="mb-3">
-                    <label for="exampleInputPassword1" class="form-label">Nama Jurusan</label>
-                    <input type="text" name="jurusan" class="form-control" id="exampleInputPassword1">
+                    <label for="exampleInputPassword1" class="form-label">Nama Lokasi</label>
+                    <input type="text" name="lokasi" class="form-control" id="exampleInputPassword1">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Tangggal</label>
+                    <input type="date" name="tanggal" class="form-control" id="exampleInputPassword1">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Jumlah Peserta</label>
+                    <input type="text" name="jumlah_peserta" class="form-control" id="exampleInputPassword1">
                 </div>
                 
                 <button type="submit" class="btn btn-primary">Submit</button>
