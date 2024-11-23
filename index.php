d961c0c18f19466ce87f6c756a55a4ccb3d00445
A <147518138+bedolls@users.noreply.github.com>
2024-11-23 09:51:16 +0700

    menambahkan modul_event

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Event Manager</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+</head>
+<body>
+<?php 
+    include_once('navbar.php');
+?>
+
+<div class="container">
+    <div class="row mt-5">
+        <div class="col-6 m-auto">
+            <div class="card">
+            <div class="card-header text-center">
+                <h3>SISTEM EVENT MANAGER</h3>
+            </div>
+            <div class="card-body text-center">
+                <h4>Selamat Datang, Event manager</h4>
+                <p>"Seorang event manager bukan hanya sekadar menyusun jadwal dan tempat, tetapi juga menciptakan pengalaman yang tak terlupakan. Keberhasilan sebuah acara bukan hanya diukur dari seberapa lancar acara tersebut berjalan, tetapi bagaimana dampaknya pada setiap orang yang hadir."
+                    Semoga kata-kata ini memberi inspirasi untuk terus bekerja dengan penuh semangat!</p>
+            </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+    <script src="js/bootstrap.js"></script>
+    <script src="js/bootstrap.bundle.js"></script>
+</body>
+</html>
---
+++ koneksi.php
@@ -0,0 +1,9 @@
+<?php
+$host = "localhost";
+$username = "root";
+$password =  "";
+$database = "uts_abdul";
+
+$koneksi = mysqli_connect($host,$username,$password,$database);
+
+?>
