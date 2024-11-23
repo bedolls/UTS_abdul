9c57b07a4da3a0821e73b7d3027e5f4149125d7e
A <147518138+bedolls@users.noreply.github.com>
2024-11-23 10:10:18 +0700

    menambah dan mengubah data di proses.php

--- modul_event/proses.php
+++ modul_event/proses.php
@@ -3,11 +3,12 @@
 include("../koneksi.php");
 
 #2. mengambil value dari form
-$kd = $_POST['kode'];
-$jrs = $_POST['jurusan'];
-
+$ne = $_POST['nama_event'];
+$lk = $_POST['lokasi'];
+$tgl = $_POST['tanggal'];
+$jml = $_POST['jumlah_peserta'];
 #3. menulis query
-$simpan = "INSERT INTO jurusans (kode,jurusan) VALUES ('$kd','$jrs')";
+$simpan = "INSERT INTO tbl_event (nama_event,lokasi,tanggal,jumlah_peserta) VALUES ('$ne','$lk','$tgl','$jml',)";
 
 #4. jalankan query
 $proses = mysqli_query($koneksi, $simpan);
